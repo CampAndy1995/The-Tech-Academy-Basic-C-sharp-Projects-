7ba7a0a5c4c34fb166f64ddda7e6f212ba5530c9
Andrew Campbell <144943158+CampAndy1995@users.noreply.github.com>
2024-02-08 13:58:38 -0800

    Add files via upload

---
+++ Programz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Assignment
+{
+    class Program
+    {
+        private const string V = "person1HoursPerWeek * person1HourlyRate);;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person1 Enter hours worked per week:");
+            var person1HoursPerWeek = Console.ReadLine();
+
+            Console.WriteLine("Person1 Enter Hourly rate:");
+            var person1HourlyRate = Console.ReadLine();
+
+            Console.WriteLine("Person2 Enter hours worked per week:");
+            var person2HoursPerWeek = Console.ReadLine();
+
+            Console.WriteLine("Person2 Enter Hourly rate:");
+            var person2HourlyRate = Console.ReadLine();
+
+            Console.WriteLine("Annual salary of Person1 is:");
+            var person1Salary = Console.WriteLine();
+
+            Console.WriteLine("Annual salary of Person2 is:");
+            var person2Salary =
+        }
+    }
+}
